38409bf07bf285eb6e93127d1ff0a4f45da56d3a
dmitry.i.sokolov <DmitryIlichSokolov@gmail.com>
2016-06-29 16:45:41 +0400

    вёрстка

--- app/src/main/java/me/ilich/mymeteringdevices/ui/about/AboutFragment.java
+++ app/src/main/java/me/ilich/mymeteringdevices/ui/about/AboutFragment.java
@@ -1,10 +1,17 @@
 package me.ilich.mymeteringdevices.ui.about;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -24,6 +31,12 @@ public class AboutFragment extends Fragment implements Titleable {
     @BindView(R.id.version)
     TextView versionTextView;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -37,6 +50,42 @@ public class AboutFragment extends Fragment implements Titleable {
         super.onViewCreated(view, savedInstanceState);
         String s = String.format(getString(R.string.about_text_3), BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE);
         versionTextView.setText(s);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.rate, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_rate) {
+            doRate();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void doRate() {
+        Uri uri = Uri.parse("market://details?id=" + getContext().getPackageName());
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                    Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET |
+                    Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        } else {
+            goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                    Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                    Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        }
+        try {
+            startActivity(goToMarket);
+        } catch (ActivityNotFoundException e) {
+            Uri uri1 = Uri.parse("http://play.google.com/store/apps/details?id=" + getContext().getPackageName());
+            startActivity(new Intent(Intent.ACTION_VIEW, uri1));
+        }
     }
 
     @Override
